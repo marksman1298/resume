31e2877e309294a876f4b81ce8f1c30c24a12e16
marksman1298 <marksaid206@gmail.com>
2022-11-07 11:41:53 -0500

    wip fixing ci/cd

---
+++ backend/create_dynamodb.py
@@ -0,0 +1,37 @@
+import boto3
+
+def create_dynamodb(table_name: str):
+    dynamodb = boto3.resource("dynamodb")
+    all_tables = [table.name for table in dynamodb.tables.all()] 
+
+    if table_name not in all_tables:
+        resume_table = dynamodb.create_table(
+            TableName=table_name,
+            KeySchema=[
+                {
+                    "AttributeName": "id",
+                    "KeyType": "HASH"
+                }
+            ],
+            AttributeDefinitions=[
+                {
+                    "AttributeName": "id",
+                    "AttributeType": "S"
+                }
+            ],
+            BillingMode='PAY_PER_REQUEST',
+        )
+
+        resume_table.wait_until_exists()
+        # resume_table.put_item(
+        #     Item={
+        #         "id": "0",
+        #         "page_views": 0
+        #     }
+        # )
+        return 0
+    return "table already exists"
+
+
+
+create_dynamodb("urmom")
